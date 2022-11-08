983788ec1785f5ae480b707346c97e21eabbad9c
syedfarhanreza <syedfarhanreza.pub16@gmail.com>
2022-11-08 21:47:44 +0600

    Router setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import router from './Router/Routes/Routes';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Hello </h2>
+    <div>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
