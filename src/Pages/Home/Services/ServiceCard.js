08e8ab2e6a75c43fc0a350f32f7ee8d49ee976ec
syedfarhanreza <syedfarhanreza.pub16@gmail.com>
2022-11-09 17:16:02 +0600

    login page added

--- src/Pages/Home/Services/ServiceCard.js
+++ src/Pages/Home/Services/ServiceCard.js
@@ -4,7 +4,7 @@ const ServiceCard = ({service}) => {
     const {image, price,name, details} = service;
     return (
         <div className="card card-compact w-96 bg-base-100 shadow-xl">
-            <figure><img className='w-full h-72' src={image} alt="Shoes" /></figure>
+            <figure><img className='w-full h-72' src={image} alt="pictures" /></figure>
             <div className="card-body">
                 <h2 className="card-title text-3xl">{name}</h2>
                 <p>{details}</p>
