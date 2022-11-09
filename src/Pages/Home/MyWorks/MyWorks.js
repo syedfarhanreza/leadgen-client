4b125c6e2fe49a7fbac42c94559907cb02e6bcf2
syedfarhanreza <syedfarhanreza.pub16@gmail.com>
2022-11-09 12:25:43 +0600

    images added in myworks section

--- src/Pages/Home/MyWorks/MyWorks.js
+++ src/Pages/Home/MyWorks/MyWorks.js
@@ -8,37 +8,40 @@ import picture6 from '../../../assets/MyWorks/picture6.jpg'
 
 const MyWorks = () => {
     return (
-        <div class="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4">
-            <div className="card w-96 bg-base-100 shadow-xl rounded">
-                <figure className="p-10 m-4 bg-white rounded">
+        <div className='mb-10'>
+            <h1 className='text-center text-5xl font-semibold text-green-600 mb-10'>--------- My Works ---------</h1>
+            <div class="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-12 justify-items-center">
+            <div className="card w-96 shadow-xl rounded">
+                <figure className="p-10  bg-white    rounded">
                     <img src={picture1} alt="picture1" className="rounded-xl" />
                 </figure>
             </div>
-            <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="card w-96 shadow-xl">
                 <figure className="p-10 bg-white rounded">
                     <img src={picture2} alt="picture2" className="rounded-xl" />
                 </figure>
             </div>
-            <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="card w-96  shadow-xl">
                 <figure className="p-10 bg-white rounded">
                     <img src={picture3} alt="picture3" className="rounded-xl" />
                 </figure>
             </div>
-            <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="card w-96 shadow-xl">
                 <figure className="p-10 bg-white rounded">
                     <img src={picture4} alt="picture4" className="rounded-xl" />
                 </figure>
             </div>
-            <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="card w-96  shadow-xl">
                 <figure className="p-10 bg-white rounded">
                     <img src={picture5} alt="picture5" className="rounded-xl" />
                 </figure>
             </div>
-            <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="card w-96 shadow-xl ">
                 <figure className="p-10 bg-white rounded">
                     <img src={picture6} alt="picture6" className="rounded-xl" />
                 </figure>
             </div>
+        </div>
         </div>
     );
 };
