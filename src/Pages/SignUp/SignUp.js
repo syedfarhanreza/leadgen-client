b4b83a7f1163ee520770805e6dccd79a72e18052
syedfarhanreza <syedfarhanreza.pub16@gmail.com>
2022-11-09 19:22:02 +0600

    user signup done

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import image from '../../assets/login/login.jpg'
+import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 
 const SignUp = () => {
-
-    const handleLogin = event => {
+    const {createUser} = useContext(AuthContext);
+    const handleSignUp = event => {
         event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        createUser(email,password)
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+        })
+        .catch(err => console.error(err));
+
     }
     return (
         <div className="hero w-full my-20">
@@ -15,7 +27,7 @@ const SignUp = () => {
             </div>
             <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 py-20">
                 <h1 className="text-5xl text-center font-bold">Sign Up</h1>
-                <form onSubmit={handleLogin} className="card-body">
+                <form onSubmit={handleSignUp} className="card-body">
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Name</span>
---
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -0,0 +1,42 @@
+import React, { createContext, useEffect, useState } from 'react';
+import app from '../../firebase/firebase.config';
+import {createUserWithEmailAndPassword, getAuth, onAuthStateChanged} from 'firebase/auth'
+
+export const AuthContext = createContext();
+const auth = getAuth(app)
+
+
+const AuthProvider = ({children}) => {
+
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    useEffect(() => {
+       const unsubcribe = onAuthStateChanged(auth, currentUser => {
+            console.log(currentUser);
+            setUser(currentUser);
+        });
+
+        return () => {
+            return unsubcribe();
+        }
+    },[]);
+
+    const authInfo ={
+        user,
+        loading,
+        createUser,
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
