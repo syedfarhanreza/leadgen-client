6f4b6716dffb8056251910ab394e15b151519121
syedfarhanreza <syedfarhanreza.pub16@gmail.com>
2022-11-09 17:54:50 +0600

    signUp page added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import image from '../../assets/login/login.jpg'
 
 const Login = () => {
@@ -12,20 +13,20 @@ const Login = () => {
                 <div className="text-center">
                     <img className='w-3/4' src={image} alt="" />
                 </div>
-                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 py-20">
                     <h1 className="text-5xl text-center font-bold">Login now!</h1>
                     <form onSubmit={handleLogin} className="card-body">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="text" placeholder="email" className="input input-bordered" />
+                            <input type="text" name='email' placeholder="email" className="input input-bordered" required/>
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="text" placeholder="password" className="input input-bordered" />
+                            <input type="password" name='password' placeholder="password" className="input input-bordered" required/>
                             <label className="label">
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                             </label>
@@ -34,6 +35,7 @@ const Login = () => {
                             <input className="btn btn-primary" type="submit" value="Login" />
                         </div>
                     </form>
+                    <p className='text-center'>Don't have an account? <Link className='text-blue-600 font-bold' to='/signup'>Sign Up</Link></p>
                 </div>
             </div>
         </div>
---
+++ src/Pages/SignUp/SignUp.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import image from '../../assets/login/login.jpg'
+
+const SignUp = () => {
+
+    const handleLogin = event => {
+        event.preventDefault();
+    }
+    return (
+        <div className="hero w-full my-20">
+        <div className="hero-content grid md:grid-cols-2 flex-col lg:flex-row">
+            <div className="text-center">
+                <img className='w-3/4' src={image} alt="" />
+            </div>
+            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 py-20">
+                <h1 className="text-5xl text-center font-bold">Sign Up</h1>
+                <form onSubmit={handleLogin} className="card-body">
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <input type="text" name='name' placeholder="Your Name" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="text" name='email' placeholder="email" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="password" name='password' placeholder="password" className="input input-bordered" required/>
+                    </div>
+                    <div className="form-control mt-6">
+                        <input className="btn btn-primary" type="submit" value="Sign Up" />
+                    </div>
+                </form>
+                <p className='text-center'>Already have an account? <Link className='text-blue-600 font-bold' to='/login'>Login</Link></p>
+            </div>
+        </div>
+    </div>
+    );
+};
+
+export default SignUp;
